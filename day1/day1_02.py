853c38cdec6dee215569b5045dc8c73a373c8247
mualchi <paul023854@gmail.com>
2017-09-20 15:09:04 +0900

    week3 file

--- day1/day1_02.py
+++ day1/day1_02.py
@@ -1,16 +1,33 @@
-f=open("data.txt","r")
-r1=f.read()
-word = r1.split()
-d={}
-for i in word:
-    if i in d.keys():
-        d[i] += 1
-    else:
-        d[i] =1
+import datetime, time
+import urllib.request
+
+url = "http://www.agkdced.org/board/bbs/board.php?bo_table=16_17&wr_id=13"
+
+f = urllib.request.urlopen(url)
+data = f.read().decode('euc-kr') # or euc-kr
+
+
+begin = data.find("친애하는")
+end = data.find("바랍니다.")
+
+end += len("바랍니다.")
+
+print("total=", len(data))
+print("begin=", begin)
+print(data[begin:begin+100])
+print(data[end-100:end])
+print("length=", end-begin)
+
+speech = data[begin:end]
+speech = speech.replace("=","") # = 을 null로 치환
+speech = speech.replace("<br/>","")
+speech = speech.split()
+#print(speech[:100])
 
 analyze = {}
-for x in word:
-    analyze[x] = analyze.get(x, 0) + 1
+for word in speech:
+    analyze[word] = analyze.get(word, 0) + 1 # get : dic 메소드
+                                             # word 가 없으면 0을 return
 
 flist = sorted(analyze.items(), key=lambda kv: kv[1], reverse=True)
 print("number of words is ", len(flist))
